7b0d79512eaa1aa0f903804bbd43a983568f4d79
Vishal Parmar <vishal.parmar@nexios.in>
2026-08-27 12:20:24 +0530

    chore: update default server port from 5000 to 5001

--- server.js
+++ server.js
@@ -199,5 +199,5 @@ function cleanWinAnsi(str) {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
